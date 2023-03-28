3c8a540e0b2cae6dc47a5a2517ae959f3a0beba6
Dorcas Opatola <66256070+DorcasOpatola@users.noreply.github.com>
2023-03-28 16:10:14 +0100

    one year summary

--- index.js
+++ index.js
@@ -7,5 +7,6 @@
 // My preferred programming language is Python.
 //
 //
-// In the next one year, I see myself creating opportunities for women and people in my network to get into tech and earn a decent living under less stressful conditions. I am now a midlevel developer with my dream work schedule, living my best life and earning cool cash.
-// I see that I am now better positioned to achieve my healthtech dreams.
+// In the next one year, I see myself creating opportunities for women and people in my network to get into tech and earn a decent living under less stressful conditions. 
+// I am now a midlevel developer with my dream work schedule, living my best life and earning cool cash.
+// I see that I am now better positioned to achieve my healthtech career goals.
